47dd136c181f4593732445fe2a7e7c475162c9aa
Abigail Collins <abigailjcollins@gmail.com>
2024-01-29 16:57:31 -0500

    JS Homework 3

---
+++ app.js
@@ -0,0 +1,43 @@
+var myOne = 45
+var myTwo = 66
+
+// Regular Function
+
+function addThem(a, b){
+    return a + b
+
+} console.log(addThem(myOne, myTwo));
+
+
+function subtractThem(a, b){
+    return a - b
+} console.log(subtractThem(myOne, myTwo));
+
+
+function multiplyThem(a, b){
+    return a * b
+} console.log(multiplyThem(myOne, myTwo));
+
+function divideThem(a, b){
+    return a / b
+} console.log(divideThem(myOne, myTwo));
+
+// Arrow Function
+
+
+const addThem = (a, b) =>{
+    return a + b
+} console.log(addThem(myOne, myTwo));
+
+const subtractThem (a, b) =>{
+    return a - b
+} console.log(subtractThem(myOne, myTwo));
+
+const multiplyThem(a, b) =>{
+    return a * b
+} console.log(multiplyThem(myOne, myTwo));
+
+const divideThem (a, b) => {
+    return a / b
+} console.log(divideThem(myOne, myTwo));
+
